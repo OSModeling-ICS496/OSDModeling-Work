2761e41361a3d6fe53eba515dd7f3fef0f642eb4
Alexander Hung <hungalex@hawaii.edu>
2024-02-21 12:15:23 -1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,7 @@ import MapComponent from './MapComponent';
 import SlidePanel from './SlidePanel';
 
 function App() {
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
     const routeData = [[19.5987, -155.4659], [19.8987, -155.8659], [20.9987, -156.9659]];
-    const togglePanel = () => setIsPanelOpen(!isPanelOpen);
 
     return (
         <div className="App">
