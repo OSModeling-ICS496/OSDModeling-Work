b052dc7b1ea91b1ae4402ebfae4eafc9579b3db0
Grayson Levy <glevy4@hawaii.edu>
2024-03-10 10:38:45 -1000

    Changed UI
    Still need to add AWS; drones need to have speed added to their capabilities

---
+++ frontend/src/CoordinateSystem.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import {
+  OutlinedInput,Box,Button,Container,InputLabel,InputAdornment,Grid,FormControl,
+} from "@mui/material";
+import Map from "./Map";
+
+const CoordinateSystem = ({ onCoordinateChange, onCoverageChange,onTimeChange }) => {
+  const [baseCoord, setBaseCoord] = useState({ lat: '', lng: '' });
+  const [reconCoord, setReconCoord] = useState({ lat: '', lng: '' });
+  const [size, setSize] = useState('');
+  const [time, setTime] = useState('');
+  const [coverage, setCoverage] = useState('');
+  const [showMap, setShowMap] = useState(false);
+
+  useEffect(() => {
+    onCoordinateChange({ baseCoord, reconCoord, size});
+    onCoverageChange(coverage);
+    onTimeChange(time)
+  }, [baseCoord, reconCoord, size, time, coverage]);
+
+  const toggleMap = () => {
+    setShowMap(!showMap);
+  };
+
+  return (
+    <Container>
+      <Box sx={{ '& > :not(style)': { m: 1 }, }} noValidate autoComplete="off">
+        <InputLabel><b>Base Location Coordinates:</b></InputLabel>
+        <Grid container spacing={2}>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Latitude"
+                required
+                value={baseCoord.lat}
+                onChange={(e) => setBaseCoord({ ...baseCoord, lat: e.target.value })}
+                endAdornment={<InputAdornment position="end">N°</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Longitude"
+                required
+                value={baseCoord.lng}
+                onChange={(e) => setBaseCoord({ ...baseCoord, lng: e.target.value })}
+                endAdornment={<InputAdornment position="end">E°</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <InputLabel><b>Recon Location Coordinates:</b></InputLabel>
+        <Grid container spacing={2}>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Latitude"
+                required
+                value={reconCoord.lat}
+                onChange={(e) => setReconCoord({ ...reconCoord, lat: e.target.value })}
+                endAdornment={<InputAdornment position="end">N°</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Longitude"
+                required
+                value={reconCoord.lng}
+                onChange={(e) => setReconCoord({ ...reconCoord, lng: e.target.value })}
+                endAdornment={<InputAdornment position="end">E°</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Size"
+                required
+                value={size}
+                onChange={(e) => setSize(e.target.value)}
+                endAdornment={<InputAdornment position="end">km²</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <InputLabel><b>Optimization Parameters:</b></InputLabel>
+        <Grid container spacing={2}>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Time"
+                required
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                endAdornment={<InputAdornment position="end">Hours</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <OutlinedInput
+                type="number"
+                placeholder="Minimum Coverage"
+                value={coverage}
+                onChange={(e) => setCoverage(e.target.value)}
+                required
+                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Button variant="contained" onClick={toggleMap}>{showMap ? 'Hide Map' : 'Show Map'}</Button>
+      </Box>
+      {showMap && (<Map coordData={[baseCoord, reconCoord]} />)}
+    </Container>
+  );
+};
+
+export default CoordinateSystem;
--- frontend/src/ShowInputs.js
+++ frontend/src/ShowInputs.js
@@ -1,81 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import {Box, Card, CardContent, Container, Grid, Paper, Typography} from "@mui/material";
+import React from 'react';
+import { Box, Card, CardContent, Container, Grid, Paper, Typography } from "@mui/material";
 
-const ShowInputs = ({ GetuavData, GettotalArea, GetminCoverage, GettimeData, GetcoordData }) => {
-    const [coordData, setCoordData] = useState({});
-    const [totalArea, settotalArea] = useState({});
-    const [timeData, setTimeData] = useState({});
-    const [minCoverage, setMinCoverage] = useState({});
-    const [uavData, setUavData] = useState({});
-
-    useEffect(() => {
-        setCoordData(GetcoordData());
-        setTimeData(GettimeData());
-        setMinCoverage(GetminCoverage());
-        settotalArea(GettotalArea());
-        setUavData(GetuavData());
-    }, [GetuavData, GettotalArea, GetminCoverage, GettimeData, GetcoordData]);
-
+const ShowInputs = ({ uavData, minCoverage, coordData, timeData }) => {
     return (
         <Box sx={{'& > :not(style)': { m: 2 },}} style={{ textAlign: 'center' }}>
+            {/* User Inpouts Displayed */}
             <Card>
                 <CardContent>
                     <Container style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
                         <Grid container spacing={1} justifyContent="center">
+                            
+                            {/* Hours */}
                             <Grid item xs={4} style={{ display: 'flex', justifyContent: 'center' }}>
                                 <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
-                                    <Typography color="#f44336" variant="h4">{timeData ? timeData.toString() : '0'}</Typography>
-                                    <Typography variant="h6">MINUTES MISSION</Typography>
+                                    <Typography color="#f44336" variant="h4">{timeData ? `${timeData}` : '0'}</Typography>
+                                    <Typography variant="h6">Hours</Typography>
+                                </Paper>
+                             </Grid>
+
+                            {/* Minimum Coverage */}
+                            <Grid item xs={4} style={{ display: 'flex', justifyContent: 'center' }}>
+                                 <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
+                                    <Typography color="#f44336" variant="h4">{minCoverage ? `${minCoverage}` : '0'} %</Typography>
+                                    <Typography variant="h6">Minimum Coverage</Typography>
                                 </Paper>
                             </Grid>
 
+                            {/* Size */}
                             <Grid item xs={4} style={{ display: 'flex', justifyContent: 'center' }}>
-                                <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
-                                    <Typography color="#f44336" variant="h4">{minCoverage ? minCoverage.toString() : '0'} %</Typography>
-                                    <Typography variant="h6">MINIMUM COVERAGE</Typography>
+                                 <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
+                                    <Typography color="#f44336" variant="h4">{coordData.size ? `${coordData.size}` : '0'} km²</Typography>
+                                    <Typography variant="h6">Recon Area Size</Typography>
                                 </Paper>
                             </Grid>
-
+                            
+                            {/* Coordinates */}
                             <Grid item xs={4} style={{ display: 'flex', justifyContent: 'center' }}>
-                              <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
-                                <Typography color="#f44336" variant="h4">{totalArea ? totalArea.toString() : '0'} km²</Typography>
-                                <Typography variant="h6">TOTAL AREA</Typography>
-                              </Paper>
+                                 <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
+                                    <Typography variant="h6">Coordinates</Typography>
+                                    <Typography variant="h6">Base:</Typography>
+                                    {coordData.baseCoord && (
+                                        <Typography color="#f44336" variant="h5">{coordData.baseCoord.lat}N, {coordData.baseCoord.lng}E</Typography>
+                                    )}
+                                         <Typography variant="h6">Recon:</Typography>
+                                    {coordData.reconCoord && (
+                                        <Typography color="#f44336" variant="h5">{coordData.reconCoord.lat}N, {coordData.reconCoord.lng}E</Typography>
+                                    )}
+                                </Paper>
                             </Grid>
                         </Grid>
                     </Container>
                 </CardContent>
             </Card>
 
-
-            <Card>
-              <CardContent>
-                <Container style={{ textAlign: 'center' }}>
-                  <Typography variant="h6" m={2}>COORDINATE</Typography>
-                  <Grid container spacing={1} justifyContent="center">
-                    {coordData && coordData.length > 0 ? (
-                        coordData.map((coord, index) => (
-                            <Grid item key={index} xs={3} style={{ display: 'flex', justifyContent: 'center' }}>
-                              <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>
-                                <Typography color="#357a38" variant="h4">
-                                  {coord.lat}N° {coord.lng}W°
-                                  <Typography color="#618833">{coord.size}km² {coord.time}mins</Typography>
-                                </Typography>
-                              </Paper>
-                            </Grid>
-                        ))
-                    ) : (
-                        <Paper style={{ width: 'fit-content', padding: '20px' }} elevation={5}>Not available</Paper>
-                    )}
-                  </Grid>
-                </Container>
-              </CardContent>
-            </Card>
-
             <Card>
                 <CardContent>
                     <Container style={{ textAlign: 'center' }}>
-                        <Typography variant="h6" m={2}>UAV COMBINATORIAL</Typography>
+                        <Typography variant="h6" m={2}>UAV's Selected</Typography>
                         <Grid container spacing={1} justifyContent="center">
                             {uavData && uavData.length > 0 ? (
                                 uavData.map((uav, index) => (
