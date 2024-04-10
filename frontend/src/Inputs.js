439ef0533a9f078ca1191fa63b09b50c7bba35df
Alexander Hung <hungalex@hawaii.edu>
2024-04-10 13:19:12 -1000

    save data drawer

--- frontend/src/Inputs.js
+++ frontend/src/Inputs.js
@@ -1,13 +1,68 @@
 import React, { useState } from 'react';
 import {
-  Card,CardContent,Box,Button,InputLabel,AccordionSummary,Typography,AccordionDetails,Accordion,Container,CssBaseline, AppBar, Toolbar, IconButton, Tooltip
+  Card,CardContent,Box,Button,InputLabel,AccordionSummary,Typography,AccordionDetails,Accordion,Container, Toolbar, IconButton, Tooltip
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import { mainListItems, secondaryListItems } from './listItems';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import MapIcon from '@mui/icons-material/Map';
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import CoordinateSystem from "./CoordinateSystem";
 import UnmannedSystems from "./UnmannedSystems";
 import ShowInputs from "./ShowInputs";
 import ShowMap from "./ShowMap";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      '& .MuiDrawer-paper': {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        boxSizing: 'border-box',
+        ...(!open && {
+          overflowX: 'hidden',
+          transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+          width: theme.spacing(7),
+          [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+          },
+        }),
+      },
+    }),
+);
 
 const Inputs = () => {
     const [coordData, setCoordData] = useState({ baseCoord: {}, reconCoord: {} });
@@ -16,6 +71,11 @@ const Inputs = () => {
     const [apiResponse, setApiResponse] = useState("");
     const [timeData, setTimeData] = useState('');
     const [showMap, setShowMap] = useState(false);
+    const [open, setOpen] = React.useState(true);
+
+    const toggleDrawer = () => {
+      setOpen(!open);
+    };
 
     const toggleMap = () => {
         setShowMap(!showMap);
@@ -84,9 +144,21 @@ const Inputs = () => {
     return (
         <div>
           <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="absolute">
-              <Toolbar>
+            <AppBar position="absolute" open={open}>
+              <Toolbar sx={{ pr: '24px', }}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={toggleDrawer}
+                    sx={{
+                      marginRight: '36px',
+                      ...(open && { display: 'none' }),
+                    }}
+                >
+                  <MenuIcon />
+                </IconButton>
+
                 <Typography
                     component="h1"
                     variant="h6"
@@ -114,6 +186,27 @@ const Inputs = () => {
                     )
                 }
             </AppBar>
+
+            <Drawer variant="permanent" open={open}>
+              <Toolbar
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    px: [1],
+                  }}
+              >
+                <IconButton onClick={toggleDrawer}>
+                  <ChevronLeftIcon />
+                </IconButton>
+              </Toolbar>
+              <Divider />
+              <List component="nav">
+                {mainListItems }
+                <Divider sx={{ my: 1 }} />
+                {secondaryListItems  }
+              </List>
+            </Drawer>
 
               <Container maxWidth="lg" sx={{ mt: 12, mb: 4 }}>
 
--- frontend/src/Outputs.js
+++ frontend/src/Outputs.js
@@ -6,17 +6,77 @@ import {
   FormControl,
   Box,
   Typography,
-  CssBaseline,
-  AppBar,
   Toolbar,
   Container,
-  Grid
+  Grid,
+  IconButton
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import { mainListItems, secondaryListItems } from './listItems';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      '& .MuiDrawer-paper': {
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        boxSizing: 'border-box',
+        ...(!open && {
+          overflowX: 'hidden',
+          transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+          width: theme.spacing(7),
+          [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+          },
+        }),
+      },
+    }),
+);
+
+
 
 const Outputs = () => {
   const [minimization, setMinimization] = useState('minimize1');
   const [mapData, setMapData] = useState([]);
   const [droneData, setDroneData] = useState({});
+  const [open, setOpen] = React.useState(true);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem('coordData'));
@@ -113,9 +173,21 @@ const Outputs = () => {
   return (
       <div>
         <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <AppBar position="absolute">
-            <Toolbar>
+          <AppBar position="absolute" open={open}>
+            <Toolbar sx={{ pr: '24px', }}>
+              <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={toggleDrawer}
+                  sx={{
+                    marginRight: '36px',
+                    ...(open && { display: 'none' }),
+                  }}
+              >
+                <MenuIcon />
+              </IconButton>
+
               <Typography
                   component="h1"
                   variant="h6"
@@ -129,7 +201,28 @@ const Outputs = () => {
             </Toolbar>
           </AppBar>
 
-          <Grid container spacing={2} sx={{ mt: 12, mb: 4 }}>
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'flex-end',
+                  px: [1],
+                }}
+            >
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              {mainListItems }
+              <Divider sx={{ my: 1 }} />
+              {secondaryListItems  }
+            </List>
+          </Drawer>
+
+          <Grid container spacing={2} sx={{ mt: 12, mb: 4}}>
             <Grid item xs={3}>
               <Container sx={{ ml: 9}}>
                 <FormControl>
@@ -152,7 +245,7 @@ const Outputs = () => {
               </Container>
             </Grid>
 
-            <Grid item xs={8} >
+            <Grid item xs={8}>
               <ShowMap coordData={mapData}/>
             </Grid>
           </Grid>
